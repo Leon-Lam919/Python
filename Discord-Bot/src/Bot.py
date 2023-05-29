ac19d19cffe4eaa5c217e4f3c1371a9c5c9876e3
Leon-Lam919 <leonlam34@gmail.com>
2023-05-29 15:37:43 -0500

    venv changed

--- Discord-Bot/src/Bot.py
+++ Discord-Bot/src/Bot.py
@@ -9,7 +9,7 @@ from discord.ext import commands, tasks
 import youtube_dl
 from dotenv import load_dotenv
 
-intents = discord.Intents.all()
+intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.bot(command_prefix='$', intents=intents)
 
