1e72c65dfb4d026d16c0cba7b4850240fa89b4ed
Leon Lam <32226530+Leon-Lam919@users.noreply.github.com>
2023-10-20 18:47:06 -0500

    -

--- Web Scraping/web.py
+++ Web Scraping/web.py
@@ -2,20 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# Collect and parse first page
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-soup = BeautifulSoup(page.text, 'html.parser')
-
-#Remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
-# Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
-
-# Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
-
-#create loop to print out all artists' names
-for artist_name in artist_name_list:
-    print(artist_name.prettify())
+url = "https://www.d20pfsrd.com/"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+for link in soup.find_all('a'):
+    print(link.get('href'))
